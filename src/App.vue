7da3fc799f1e5ade7f43288af1072156764b5f9d
Eduardo Rayón <rayon@MacBook-Pro-de-Eduardo.local>
2018-12-21 09:50:09 -0600

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,143 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <VApp>
+    <!-- <VToolbar app>
+      <VToolbarTitle class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </VToolbarTitle>
+      <VSpacer></VSpacer>
+      <VBtn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </VBtn>
+    </VToolbar> -->
+
+    <VNavigationDrawer
+      v-model="drawer"
+      :mini-variant="mini"
+      dark
+      fixed
+      app
+      width="250"
+      permanent
+    >
+      <VList class="pa-1">
+        <VListTile v-if="mini" @click.stop="mini = !mini">
+          <VListTileAction>
+            <VIcon>chevron_right</VIcon>
+          </VListTileAction>
+        </VListTile>
+
+        <VListTile avatar tag="div">
+          <VListTileAvatar >
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </VListTileAvatar>
+
+          <VListTileContent>
+            <VListTileTitle>Gruppe ZEBRA</VListTileTitle>
+          </VListTileContent>
+
+          <VListTileAction>
+            <VBtn icon @click.stop="mini = !mini">
+              <VIcon>chevron_left</VIcon>
+            </VBtn>
+          </VListTileAction>
+        </VListTile>
+      </VList>
+
+      <!-- <VToolbar flat>
+        <VList v-if="!mini">
+          <VListTile>
+            <VListTileTitle class="title">
+              ZEBRA PRINTER
+            </VListTileTitle>
+          </VListTile>
+        </VList>
+        <VToolbarItems>
+          <VBtn icon @click.stop="mini = !mini">
+            <VIcon>chevron_left</VIcon>
+          </VBtn>
+        </VToolbarItems>
+      </VToolbar> -->
+
+
+      <VList class="pt-0">
+        <VDivider light></VDivider>
+
+        <VListTile
+          v-for="item in menu"
+          :key="item.title"
+          :to="{ name: item.name }"
+          exact
+        >
+          <VListTileAction>
+            <VIcon>{{ item.icon }}</VIcon>
+          </VListTileAction>
+
+          <VListTileContent>
+            <VListTileTitle>{{ item.title }}</VListTileTitle>
+          </VListTileContent>
+        </VListTile>
+      </VList>
+    </VNavigationDrawer>
+
+    <VContent>
+      <Transition name="slide" mode="out-in">
+        <RouterView />
+      </Transition>
+    </VContent>
+
+    <Snackbar />
+  </VApp>
 </template>
 
+<script>
+import menu from '@/api/menu';
+
+export default {
+  name: 'App',
+  components: { Snackbar: () => import('@/components/Snackbar') },
+  data() {
+    return {
+      drawer: null,
+      menu,
+      mini: false,
+    };
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
---
+++ src/api/menu.js
@@ -0,0 +1,19 @@
+const Menu = [
+  {
+    title: 'Etiquetas',
+    icon: 'label',
+    name: 'Home'
+  },
+  {
+    title: 'Impresión',
+    icon: 'print',
+    name: 'About'
+  },
+  // {
+  //   title: 'Usuarios',
+  //   icon: 'notifications',
+  //   name: 'users'
+  // }
+];
+
+export default Menu;
