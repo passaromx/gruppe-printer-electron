cfeaee0e00ccc0ad369d7e33a42657d21b7d613f
pmcarlos <carlos@passaromx.com>
2019-01-23 09:51:56 -0600

    Fixed variable-wrapper

--- src/utils/offline/printer/zplFormat.js
+++ src/utils/offline/printer/zplFormat.js
@@ -1,8 +1,11 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable no-useless-escape */
 module.exports = (format, params) => {
+  console.log(format);
+
   let start = '^XA';
   if (format === 1) {
+    console.log('1', params);
     const { description, expireDate, productionDate } = params;
     start = `^XA
       ^MMC
@@ -13,16 +16,19 @@ module.exports = (format, params) => {
       ^FT301,680^A0I,31,31^FH\^FD${expireDate || ''}^FS
       ^FT301,765^A0I,31,31^FH\^FD${productionDate || ''}^FS^LS0`;
   } else if (format === 2) {
+    console.log('2', params);
     const { description, weight, date } = params;
     start = `^XA
       ^MMC
-      ^PW831
-      ^LL1574
+      ^PW839
+      ^LL1594
       ^LS0
-      ^FT125,1562^A0I,31,31^FH\^FD${weight} KG^FS
-      ^FT365,1520^A0I,31,31^FH\^FD${description}^FS
-      ^FT8,311^A0R,20,21^FH\^FD${date}^FS
-      ^FT815,165^A0R,15,15^FPH\^FD${description}^FS^LS0`;
+      ^CFO,100
+      ^FT125,1562^A@I,25,25,ARIALBOLD.FNT^FD${weight} KG^FS
+      ^FT365,1523^A@I,33,33,ARIAL.FNT^FD${description}^FS
+
+      ^FT8,311^A@R,19,19,ARIALBOLD.FNT^FD${date}^FS
+      ^FT830,280^A@B,23,23,ARIALBOLD.FNT^FD${description}^FS^LS0`;
   }
   return start;
 };
