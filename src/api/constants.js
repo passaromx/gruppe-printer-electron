e23eb8c9f603bf97b469798220ede662098a819c
rayonef <eduardo.rayon1@gmail.com>
2019-08-05 12:35:09 -0500

    added item 'TEX' to factories array

--- src/api/constants.js
+++ src/api/constants.js
@@ -21,7 +21,7 @@ export const rowsPerPage = [15, 25, 50, {
 }];
 
 const maltaFactories = [
-  'BAJ', 'CLN', 'GDL', 'MER', 'MOR', 'MTY', 'STJ', 'TEP', 'TLX'
+  'BAJ', 'CLN', 'GDL', 'MER', 'MOR', 'MTY', 'STJ', 'TEP', 'TEX', 'TLX'
 ];
 
 export const maltaVars = {
