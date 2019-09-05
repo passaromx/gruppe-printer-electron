3dce9c873014bb7734a694c5e0666a09ee66f9e0
rayonef <eduardo.rayon1@gmail.com>
2019-09-05 01:08:53 -0500

    api url fix

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,6 @@
 
 // export const filesURL = 'http://localhost:1337';
-export const filesURL = 'http://3.81.203.178:1337';
+export const filesURL = 'http://34.239.219.72:1337';
 
 export const apiURL = `${filesURL}/`;
 
