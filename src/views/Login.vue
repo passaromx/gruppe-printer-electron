d3451f3d7ef74fa1ea7007bc96dc7a026226cfd4
rayonef <eduardo.rayon1@gmail.com>
2019-11-05 18:15:00 -0600

    v1.2.5 ready

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -43,7 +43,7 @@
           </VForm>
 
           <VLayout row justify-end class="mt-5">
-            <span class="caption">v1.2.4</span>
+            <span class="caption">v1.2.5</span>
           </VLayout>
 
         </VLayout>
