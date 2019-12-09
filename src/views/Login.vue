e39c42d5635ba9d2f633e12b7ee5c23c7efa7531
rayonef <eduardo.rayon1@gmail.com>
2019-12-09 10:45:09 -0600

    avoid rendering empty flex containers if var comes client settings

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -43,7 +43,7 @@
           </VForm>
 
           <VLayout row justify-end class="mt-5">
-            <span class="caption">v1.2.6</span>
+            <span class="caption">v1.2.7</span>
           </VLayout>
 
         </VLayout>
